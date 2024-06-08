dee934acc8993c1a110c52a92f7637b0a88158f9
roudayna <roudaynacherif9@gmail.com>
2024-06-08 01:20:02 +0100

    first front

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Tutorial/>
+    <div class="container">
+      <div>
+        <h1 class="title">
+          Hello explorer
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Tutorial: () => import('@/components/Tutorial.vue') // Assurez-vous que le chemin est correct
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
